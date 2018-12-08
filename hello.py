b0dae2f18006ce47bece8dddc9e0ce81f8a6114d
Stephen Muth <stephen.muth@oracle.com>
2018-12-07 18:20:49 -0700

    SJM_BCSC28020411

--- hello.py
+++ hello.py
@@ -6,3 +6,6 @@ print('Hello, World.')
 
 #Change 2 of Feature_1
 print('This is python version {}'.format(platform.python_version()))
+
+#Featue branch off of Working
+print('This feature was added from working. BCSC-Bug 28020411')
