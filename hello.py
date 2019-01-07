2f36ff782d0baf3b13ea255b69703ca3dc78775c
User One <user.one@fakemail.org>
2019-01-06 21:27:42 -0700

    Merge conflict demo.

--- hello.py
+++ hello.py
@@ -7,4 +7,7 @@ print('Hello, World.')
 #Change 2 of Feature_1
 print('This is python version {}'.format(platform.python_version()))
 
+#This entry is to deomonstrate a merge conflict.
+var1=7.0
+print(var1)
 
