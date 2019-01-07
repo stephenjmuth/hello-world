aa2b6f6524f4a14ed2907ed247f0dc73d2e9c44f
User One <user.one@fakemail.org>
2019-01-06 21:20:11 -0700

    reset

--- hello.py
+++ hello.py
@@ -7,6 +7,4 @@ print('Hello, World.')
 #Change 2 of Feature_1
 print('This is python version {}'.format(platform.python_version()))
 
-#This entry is to deomonstrate a merge conflict.
-var1=7.0
-print(var1)
+
