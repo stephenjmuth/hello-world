84e157148db6d85c485b33e91ed41b7f2ea61864
Stephen Muth <stephen.muth@oracle.com>
2019-01-28 21:39:09 -0700

    Some Commit message.

--- hello.py
+++ hello.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
-#Change 1 of Feature_1
 import platform
 print('Hello, World.')
 
-#Change 2 of Feature_1
 print('This is python version {}'.format(platform.python_version()))
