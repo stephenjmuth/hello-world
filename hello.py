d6dbec2971b0a10ddfce1a95efb409d16ba2e6e3
Stephen Muth <stephen.muth@oracle.com>
2019-02-21 16:10:48 -0700

    testing revert

--- hello.py
+++ hello.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
+####################################################################################################
 
 import platform
-print('Hello, World.')
 
-print('This is python version {}'.format(platform.python_version()))
+print("Hello, World.")
 
-#This entry is to deomonstrate a merge conflict.
-var1=7.0
+print(
+    "This is python version {}".format(
+        platform.python_version()
+    )
+)
+
+# This entry is to deomonstrate a merge conflict.
+var1 = 7.0
 print(var1)
-
